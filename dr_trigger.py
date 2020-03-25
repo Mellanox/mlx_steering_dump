3f32eab96d238b63786cbfea038b532f261cea2a
Muhammad Sammar <muhammads@mellanox.com>
2020-03-25 10:48:40 +0200

    Add dump trigger Support for Python2
    Implemented file descriptor sharing mechanism so dump triggering can be
    performed both in Py 2 and 3 (since Py2 doesn't support
    socket.sendmsg())

--- dr_trigger.py
+++ dr_trigger.py
@@ -1,63 +1,111 @@
-# Copyright (c) 2020 Mellanox Technologies, Inc.  All rights reserved.
-#
-# This software is available to you under a choice of one of two
-# licenses.  You may choose to be licensed under the terms of the GNU
-# General Public License (GPL) Version 2, available from the file
-# COPYING in the main directory of this source tree, or the
-# OpenIB.org BSD license below:
-#
-#     Redistribution and use in source and binary forms, with or
-#     without modification, are permitted provided that the following
-#     conditions are met:
-#
-#      - Redistributions of source code must retain the above
-#        copyright notice, this list of conditions and the following
-#        disclaimer.
-#
-#      - Redistributions in binary form must reproduce the above
-#        copyright notice, this list of conditions and the following
-#        disclaimer in the documentation and/or other materials
-#        provided with the distribution.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-import io
-import os
 import socket
-import struct
 import sys
+import os
+import struct
+from ctypes import *
 
+# struct iovec {
+#    void  *iov_base;    /* Starting address */
+#    size_t iov_len;     /* Number of bytes to transfer */
+# };
+class iovec(Structure):
+    _fields_ = [('iov_base', c_void_p), ('iov_len', c_size_t)]
+
+# struct msghdr {
+# 	void		*msg_name;	/* ptr to socket address structure */
+# 	int		msg_namelen;	/* size of socket address structure */
+# 	struct iov_iter	msg_iter;	/* data */
+# 	void		*msg_control;	/* ancillary data */
+# 	__kernel_size_t	msg_controllen;	/* ancillary data buffer length */
+# 	unsigned int	msg_flags;	/* flags on received message */
+# 	struct kiocb	*msg_iocb;	/* ptr to iocb for async requests */
+# };
+class msghdr(Structure):
+    _fields_ = [('msg_name', c_void_p), ('msg_namelen', c_uint), ('msg_iov', POINTER(iovec)), ('msg_iovlen', c_size_t),
+                ('msg_control', c_void_p), ('msg_controllen', c_size_t), ('msg_flags', c_int)]
+
+# struct cmsghdr {
+#    size_t cmsg_len;    /* Data byte count, including header
+# 						  (type is socklen_t in POSIX) */
+#    int    cmsg_level;  /* Originating protocol */
+#    int    cmsg_type;   /* Protocol-specific type */
+# /* followed by
+#   unsigned char cmsg_data[]; */
+# };
+class cmsghdr(Structure):
+    _fields_ = [('cmsg_len', c_size_t), ('cmsg_level', c_int), ('cmsg_type', c_int)]
+
+    @classmethod
+    def create(cls, cmsg_len, cmsg_level, cmsg_type, cmsg_data):
+        CHAR_ARRAY = c_ubyte * sizeof(cmsg_data)
+        cmsg_data = CHAR_ARRAY(*bytearray(cmsg_data))
+
+        class cmsghdr_with_data(Structure):
+            _fields_ = cls._fields_ + [('cmsg_data', CHAR_ARRAY)]
+
+        return cmsghdr_with_data(cmsg_len, cmsg_level, cmsg_type,cmsg_data)
+
+#define CMSG_LEN(len) (sizeof(struct cmsghdr) + (len))
+def CMSG_LEN(c_len):
+    return c_size_t(sizeof(cmsghdr) + c_len)
+
+
+def fd_msg(fd, port, prevent_py_gc):
+    fd = c_int(fd)
+    port = bytearray(chr(port), 'utf-8') + bytearray('\0', 'utf-8')
+
+    # create c bytes buffer of size iov_len that contains the port as string
+    iov_len = c_int * 20
+    iov_base = iov_len(*port)
+
+    ptr_iovec = POINTER(iovec)
+    iov = iovec(addressof(iov_base),c_size_t(sizeof(iov_base)))
+    prevent_py_gc['iov'] = iov
+    prevent_py_gc['iov_base'] = iov_base
+
+    SCM_RIGHTS = 0x01
+    struct_cmsghdr = cmsghdr.create(CMSG_LEN(sizeof(fd)), socket.SOL_SOCKET, SCM_RIGHTS, fd)
+    prevent_py_gc['cmsghdr'] = struct_cmsghdr
+
+    return msghdr(None, 0, ptr_iovec(iov), 1, addressof(struct_cmsghdr), c_size_t(sizeof(struct_cmsghdr)))
 
 def connect_to_server(server_pid):
     path = "/var/tmp/dpdk_net_mlx5_%d" % server_pid
-    if os.path.exists(path) is False:
-        print("DPDK doesn't support steering dump trigger", file=sys.stderr)
-        return None
+    if(os.path.exists(path) == False):                                      
+        print("DPDK doesn't support steering dump trigger")
+        sys.exit(1)
 
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     try:
         sock.connect(path)
     except OSError as msg:
-        print("failed to connect to DPDK server: %s" % msg, file=sys.stderr)
-        return None
+        print("failed to connect to DPDK server: %s" % msg)
+        sys.exit(1)
     return sock
 
+def request_dump(sock, port, dump_file):
+    try:
+        #link to c code of sendmsg
+        libc = CDLL('libc.so.6')
+    except:
+        print("failed to link function sendmsg from libc.so.6")
+        return
 
-def request_dump(sock, port, fd):
-    sock.sendmsg([struct.pack('H', port)],
-                 [(socket.SOL_SOCKET, socket.SCM_RIGHTS, struct.pack('i', fd))])
-    msg, ancdata, flags, addr = sock.recvmsg(struct.calcsize('i'))
-    ret = struct.unpack('i', msg)[0]
-    if ret:
-        print("server error: %s" % os.strerror(ret))
+    c_sendmsg = libc.sendmsg
+    c_sendmsg.argtypes = [c_int, POINTER(msghdr), c_int]
+    c_sendmsg.restype = c_int
 
+    #this variable used to prevent python garbage collector from disposing some resources.
+    prevent_py_gc = {}
+
+    sock_num = c_int(sock.fileno())
+    msg = fd_msg(dump_file.fileno(), port, prevent_py_gc)
+
+    ret = c_sendmsg(sock_num, msg, 0)
+    if ret == -1:
+        print("failed to request dump")
+
+    sock.recv(1024)
 
 def trigger_dump(s_pid, s_port, path):
     global port
@@ -67,18 +115,13 @@ def trigger_dump(s_pid, s_port, path):
     server_pid = s_pid
 
     try:
-        file = io.open(path, 'w')
+        dump_file = open(path , 'w')
     except IOError as msg:
-        print("failed to open dump file: %s" % msg, file=sys.stderr)
-        return None
+        print("failed to open dump file: %s" % msg)
+        sys.exit(1)
 
     sock = connect_to_server(server_pid)
-    if sock is None:
-        file.close()
-        return None
-
-    request_dump(sock, port, file.fileno())
-
-    file.close()
+    request_dump(sock, port, dump_file)
+    dump_file.close() 
     sock.close()
-    return path
+    return path 
