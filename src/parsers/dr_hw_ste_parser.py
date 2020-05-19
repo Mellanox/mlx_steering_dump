ed5d62ce60d3076594f94aa384e6fa02f4a6c3ca
Muhammad Sammar <muhammads@mellanox.com>
2020-05-19 16:21:12 +0300

    Add STE tag parser for "flex_parser_1"
    This helps parsing STEs with a tag of type flex_parser_1
    
    Signed-off-by: Muhammad Sammar <muhammads@mellanox.com>

--- src/parsers/dr_hw_ste_parser.py
+++ src/parsers/dr_hw_ste_parser.py
@@ -483,6 +483,15 @@ def mlx5_ifc_ste_flex_parser_0_bits_tag_parser(bin_str):
     return ret
 
 
+def mlx5_ifc_ste_flex_parser_1_bits_tag_parser(bin_str):
+    ret = {}
+    ret["flex_parser_7"] = _val(bin_str[0: 32])
+    ret["flex_parser_6"] = _val(bin_str[32: 64])
+    ret["flex_parser_5"] = _val(bin_str[64: 96])
+    ret["flex_parser_4"] = _val(bin_str[96: 128])
+    return ret
+
+
 def mlx5_tag_parser(lookup_type, tag, raw):
     switch = {"0x05": [mlx5_ifc_ste_src_gvmi_qp_bits_tag_parser, False],
               "0x0a": [mlx5_ifc_ste_eth_l2_tnl_bits_tag_parser_p, True],
@@ -518,6 +527,7 @@ def mlx5_tag_parser(lookup_type, tag, raw):
               "0x2f": [mlx5_ifc_ste_register_0_bits_tag_parser, False],
               "0x30": [mlx5_ifc_ste_register_1_bits_tag_parser, False],
               "0x22": [mlx5_ifc_ste_flex_parser_0_bits_tag_parser, False],
+              "0x23": [mlx5_ifc_ste_flex_parser_1_bits_tag_parser, False],
               }
 
     if lookup_type not in switch.keys():
