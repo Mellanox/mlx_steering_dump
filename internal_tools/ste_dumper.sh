50ee60f6027ff150965c698bb440c087b255e4e4
Hamdan Igbaria <hamdani@nvidia.com>
2021-01-06 17:49:55 +0200

    Add new tool "ste_dumper'
    Add new tool to support icm memory single index dump.
    Usage: ./ste_dumper.sh -h
    
    Signed-off-by: Hamdan Igbaria <hamdani@nvidia.com>

---
+++ internal_tools/ste_dumper.sh
@@ -0,0 +1,343 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: (GPL-2.0 OR Linux-OpenIB)
+# Copyright (c) 2020 Nvidia, Inc. All rights reserved. See COPYING file
+
+###### Initial values
+mst_dev="-1"
+gvmi="-1"
+portid="-1"
+size=1
+index=-1
+res_type=0
+pretty=0
+res_dump_supp=0
+port="0"
+tool=""
+parsing="raw"
+verbosity="0"
+ste_version="" #CONNECTX_5=0x0, CONNECTX_6DX=0x1
+
+icmd_opcode_busy_addr="0x23fc.0:32"
+icmd_busy_addr="0x23fc.0:1"
+icmd_status_addr="0x23fc.8:8"
+opcode="8001"
+icmd_start_addr="0x2000"
+icmd_gvmi_addr="0x2040.0:16"
+icmd_rw_addr="0x2040.31:1"
+icmd_ctx_type_addr="0x2044.0:16"
+icmd_index_hi_addr="0x2048.0:32"
+icmd_index_lo_addr="0x204c.0:32"
+
+function ste_parser {
+	local ste_raw=$1
+
+	python ste_dumper_python.py $ste_version $ste_raw $index $verbosity
+}	
+
+###### read_res_icmd
+# Read resource using icmd
+# NOTE: Resource type (res_type) is not guaranteed to be compatible between FW.
+############
+function read_res_icmd {
+	local mst_dev=$1
+	local gvmi=$2
+	local res_type=$3
+	local res_num=$4
+
+	sudo mcra $mst_dev $icmd_gvmi_addr	  $gvmi
+	sudo mcra $mst_dev $icmd_rw_addr	  1
+	sudo mcra $mst_dev $icmd_ctx_type_addr	  $res_type
+	sudo mcra $mst_dev $icmd_index_hi_addr	  $(( res_num >> 32 ))
+	sudo mcra $mst_dev $icmd_index_lo_addr	  $(( res_num & 0xffffffff ))
+
+	sudo mcra $mst_dev $icmd_opcode_busy_addr 0x${opcode}0001
+
+	sleep 0.5
+	local busy=`sudo mcra $mst_dev $icmd_busy_addr`
+	while [ $(( busy )) -ne 0 ] ; do busy=`sudo mcra $mst_dev $icmd_busy_addr`;  sleep 0.1 ; done
+
+	local status=`sudo mcra $mst_dev $icmd_status_addr`
+	if [ "$status" != "0x00000000" ] ; then
+		echo  "ERROR: icmd failed, (status=$status);"
+		return;
+	fi
+}
+
+###### get_gvmi_counter
+# Read counter using icmd
+############
+function get_gvmi_counter {
+	local l_mst_dev=$1
+	local l_port=$2
+	local l_gvmi=$3
+	local l_rx_sx_=$4
+	local l_cntr_ix=$5
+	local l_ctx_type="0x1b"
+	
+	if [ "$l_rx_sx_" -eq "1" ] ; then
+		trns_type="RX"
+		if [ "$l_port" -eq "0" ] ; then
+			l_ctx_type="0x18"
+		else
+			l_ctx_type="0x1f"
+		fi
+	else
+		trns_type="TX"
+		if [ "$l_port" -eq "0" ] ; then
+			l_ctx_type="0x36"
+		else
+			l_ctx_type="0x37"
+		fi
+	fi
+	
+	mcra $l_mst_dev $icmd_gvmi_addr        $l_gvmi
+	mcra $l_mst_dev $icmd_ctx_type_addr    $l_ctx_type
+	mcra $l_mst_dev $icmd_rw_addr	       0x1
+	mcra $l_mst_dev $icmd_index_hi_addr    0x0
+	mcra $l_mst_dev $icmd_index_lo_addr    $l_cntr_ix
+	mcra $l_mst_dev $icmd_opcode_busy_addr 0x80010001
+	sleep 0.5
+	local busy=`mcra $l_mst_dev $icmd_busy_addr`
+	while [ $(( busy )) -ne 0 ] ; do busy=`mcra $l_mst_dev $icmd_busy_addr`;  sleep 0.1 ; done
+	
+	local status=`mcra $l_mst_dev $icmd_status_addr`
+	if [ "$status" != "0x00000000" ] ; then
+	    echo  "ERROR: icmd failed, (status=$status);"
+	    exit;
+	fi
+	
+	local bytes_a=`mcra $l_mst_dev $(( icmd_start_addr))`
+	local bytes_b=`mcra $l_mst_dev $(( icmd_start_addr + 4))`
+	local bytes_c=$(((bytes_a << 32) + bytes_b))
+	
+	local packets_a=`mcra $l_mst_dev $(( icmd_start_addr + 8))`
+	local packets_b=`mcra $l_mst_dev $(( icmd_start_addr + 8 + 4))`
+	local packets_c=$(((packets_a << 32) + packets_b))
+
+	case "$l_cntr_ix" in
+		0)      cntr_string="DONT_CARE    " ;;
+		1)      cntr_string="ERRORS       " ;;
+		2)      cntr_string="IB_UNICAST   " ;;
+		3)      cntr_string="IB_MUTICAST  " ;;
+		4)      cntr_string="ETH_BROADCAST" ;;
+		5)      cntr_string="ETH_UNICAST  " ;;
+		6)      cntr_string="ETH_MULTICAST" ;;
+		*)	cntr_string="FLOW_COUNTER " ;;
+	esac
+
+	if [ "$parsing" == "parsed" ] ; then
+		printf "cntr[%d] %s - %14.35s : 0x%.16x  |  0x%.16x\n" $l_cntr_ix $trns_type $cntr_string $bytes_c $packets_c
+	else
+		printf "%.8x\n%.8x\n%.8x\n%.8x" $bytes_a $bytes_b $packets_a $packets_b
+	fi
+
+	for i in {0..3} ; do
+		mcra $l_mst_dev $((icmd_start_addr + i*4)) 0x0
+	done
+}
+
+###### read_res_icmd_and_print
+# Read run icmd to read resourse and print the output
+############
+function read_res_icmd_and_print {
+	local mst_dev=$1
+	local gvmi=$2
+	local res_type=$3
+	local index=$4
+	local port=$5
+	local txt=""
+	local i
+
+	if [ "$action" == "counter" ]; then
+		local icmd_start_addr="0x2000"
+		for i in {0..15} ; do
+			mcra $mst_dev $((icmd_start_addr + i*4)) 0x0
+		done
+
+		rx_counter=`get_gvmi_counter $mst_dev $port $gvmi 1 $index`
+		sx_counter=`get_gvmi_counter $mst_dev $port $gvmi 0 $index`
+
+		if [ "$parsing" == "parsed" ]; then
+			rx_p=`echo $rx_counter | awk '{print $6}'`
+			rx_b=`echo $rx_counter | awk '{print $8}'`
+			sx_p=`echo $sx_counter | awk '{print $6}'`
+			sx_b=`echo $sx_counter | awk '{print $8}'`
+			total_p=`printf "0x%x" $(( rx_p + sx_p ))`
+			total_b=`printf "0x%x" $(( rx_b + sx_b ))`
+			echo "                           :     Packets        |      Bytes         "
+			echo "-----------------------------------------------------------------------"
+			echo $rx_counter
+			echo "-----------------------------------------------------------------------"
+			echo $sx_counter
+			echo "-----------------------------------------------------------------------"
+			printf "TOTAL                      : 0x%.16x | 0x%.16x\n" $total_p $total_b
+			echo
+		else
+			echo $rx_counter
+			echo $sx_counter
+		fi
+		exit
+	fi
+
+	if [ "$action" == "ste" ]; then
+		txt=`resourcedump dump -d $mst_dev --segment STE_RANGE --index1 $index --num-of-obj1 1`
+		txt=`echo "$txt" | awk '/Segment Type\: 0x1310/{nr[NR + 4]; nr[NR + 5]; nr[NR + 6]; nr[NR + 7];}; NR in nr' | sed 's/0x//g'`
+		echo "$txt"
+		if [ "$parsing" == "parsed" ]; then
+			txt=`echo $txt | sed 's/ //g'`
+			ste_parser $txt
+		fi
+		exit
+	fi
+
+	if [ "$action" == "rewrite" ] && [ "$tool" == "mft" ] ; then
+		txt=`resourcedump dump -d $mst_dev --segment MDFY_HDR_RNG --index1 $index --num-of-obj1 1`
+		if [ "$parsing" == "raw" ]; then
+			txt=`echo "$txt" | awk '/Segment Type\: 0x1020/{nr[NR + 3]; nr[NR + 4]; nr[NR + 5]; nr[NR + 6]; nr[NR + 7]; nr[NR + 8]; nr[NR + 9]; nr[NR + 10]; nr[NR + 11];}; NR in nr' | sed 's/0x//g'`
+		fi
+		echo "$txt"
+		exit
+	fi
+
+	read_res_icmd $mst_dev $gvmi $res_type $(( index >> 1 ))
+
+	for i in {0..32} ; do
+		txt=`mcra $mst_dev $((0x2000 + (i*4) )) | sed 's/0x//'`
+		echo "$txt"
+	done
+}
+
+
+for arg in "$@"
+do
+	case "$arg" in
+		-h)	echo ""
+			echo "############## STE DUMPER ##################"
+			echo
+			echo "Disclaimer: this tool is for internal use for NVIDIA developers."
+			echo "This tool is used to dump the steering data from the relevant device."
+			echo "This tool will present raw data and has the ability to parse some of the data."
+			echo
+			echo  Usage:
+			echo "	./ste_dumper.sh -d <mst_dev> -g <gvmi> -i <index> <resource types> [--parsed|-p] [--mft|--mcra] [-h] [--verbose|-v]"
+			echo
+			echo "Required flags:"
+			echo "	-d <mst_dev> "
+			echo "	-g <gvmi>"
+			echo "	-i <index>"
+			echo
+			echo "Resource types:"
+			echo "	--encap                   dump encap header"
+			echo "	--rewrite                 dump header rewrite"
+			echo "	--pattern                 dump pattern"
+			echo "	--argument                dump argument"
+			echo "	--counter                 dump counter"
+			echo "	--ste                     dump ste"
+			echo
+			echo "Optional:"
+			echo "	--mft       use commands just via mft tools"
+			echo "	--mcra      use commands just via mcra interface"
+			echo "	For --mft or --mcra if not specified will use automaticlly whats available"
+			echo
+			echo "	--parsed | -p   parse the ste raw data, for now this supported just for counter parsing and STE"
+			echo
+			echo "	--verbose | -v  output extra prints"
+			echo
+			echo "	-h output help"
+			echo
+			echo "##########################################"
+			exit 1
+			;;
+
+		-i) p_arg=${arg##"-"} ;;
+		-d) p_arg=${arg##"-"} ;;
+		-g) p_arg=${arg##"-"} ;;
+		--rewrite) action="rewrite" ;   res_type=0xe9  ;;
+		--encap) action="encap" ;       res_type=0xb3  ;;
+		--pattern) action="pattern" ;   res_type=0x15d ;;
+		--argument) action="argument" ; res_type=0x15c ;;
+		--counter) action="counter" ;	res_type=0x4fi ;;
+		--ste) action="ste" ;		res_type=0x41  ;;
+		--mft) tool="mft" ;;
+		--mcra) tool="mcra" ;;
+		--parsed|-p) parsing="parsed" ;;
+		--verbose|-v) verbosity="1";;
+		 *) case $p_arg in
+			i) index="$arg" ; p_arg=""  ;;
+			d) mst_dev="$arg" ; p_arg=""  ;;
+			g) gvmi="$arg" ; p_arg=""  ;;
+			p) port="$arg" ; p_arg=""  ;;
+			*) echo  ERROR: bad params $p_arg $arg; exit;;
+			esac
+	esac
+done
+
+index=`printf "%d" $index`
+
+if [ "$mst_dev" == "-1" ] ; then 
+	echo  "ERROR: missing mst_dev";
+	exit 1;
+else
+	if [[ $mst_dev == *"4119"* || $mst_dev == *"4121"* || $mst_dev == *"41682"* ]]; then
+		ste_version=0x0
+	fi
+
+	if [[ $mst_dev == *"4125"* || $mst_dev == *"41686"* ]]; then
+		ste_version=0x1
+	fi
+fi
+
+if [ "$gvmi" == "-1" ] ; then 
+	echo  ERROR: missing gvmi;
+	exit 1;
+fi
+
+if [ "$res_type" == "0" ] ; then 
+	echo  ERROR: invalid res_type;
+	exit 1;
+fi
+
+if [ "$index" == "-1" ] ; then 
+	echo  ERROR: missing index;
+	exit 1;
+fi
+
+if [ "$tool" == "mcra" ] && [ "$action" == "ste" ] ; then
+        echo  ERROR: STE dumping is not supported via mcra;
+        exit 1;
+fi
+
+if [[ "$tool" == "mft" && "$action" != "ste" && "$action" != "rewrite" ]] ; then
+        echo  ERROR: $action dumping is not supported via mft;
+        exit 1;
+fi
+
+if [[ "$parsing" == "parsed" && "$action" != "counter" && "$action" != "ste" ]] ; then
+	echo  ERROR: parsed mode is not supported for $action;
+	exit 1;
+fi
+
+if [ -x "$(command -v resourcedump)" ]; then
+	res_dump_supp=1;
+fi
+
+if [ "$action" == "ste" ] && [ "$res_dump_supp" != "1" ] ; then
+	echo  ERROR: STE parsing is not supported without resourcedump;
+	exit 1;
+fi 
+
+index=`printf "0x%x" $index`
+gvmi=`printf "0x%x" $gvmi`
+
+if [ "$verbosity" != "0" ] ; then
+	echo
+	echo "##############################################################"
+	echo "Read type: $res_type action: $action index: $index gvmi: $gvmi"
+	echo "##############################################################"
+fi
+
+read_res_icmd_and_print $mst_dev $gvmi $res_type $index $port
+
+exit
+
---
+++ internal_tools/ste_dumper_python.py
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: (GPL-2.0 OR Linux-OpenIB)
+# Copyright (c) 2020 Nvidia, Inc. All rights reserved. See COPYING file
+
+import sys
+import math
+from src.parsers.dr_ste_parser import mlx5_hw_ste_parser
+
+ste = mlx5_hw_ste_parser(int(sys.argv[1], 16), sys.argv[2], 1, 0)
+
+next_table_base_31_5_size = 0
+
+if 'next_table_base_31_5_size' in ste:
+    next_table_base_31_5_size = int(ste['next_table_base_31_5_size'], 16)
+
+next_log_size = next_table_base_31_5_size & (~(next_table_base_31_5_size - 1)) #get first non zero bit
+
+print('\n##############################################\n')
+if 'entry_type' in ste:
+    print('\tentry_type: ' + ste['entry_type'])
+
+if 'next_table_base_39_32_size' in ste:
+    print('\thit_address: ' + (hex(((int(ste['next_table_base_39_32_size'], 16) << 27) | (next_table_base_31_5_size & (~next_log_size))) >> 1)))
+
+if 'miss_address_31_6' in ste and 'miss_address_39_32' in ste:
+    print('\tmiss_address: ' + hex(int(ste['miss_address_31_6'], 16) + (int(ste['miss_address_39_32'], 16) << 26)))
+
+if int(sys.argv[1], 16) == 0:
+    print('\tentry_sub_type: ' + ste['entry_sub_type'])
+    print('\tnext_lu_type: ' + ste['next_lu_type'])
+
+if next_log_size > 0:
+    print('\tnext_log_size: ' + hex(int(math.ceil(math.log(next_log_size, 2)))))
+else:
+    print('\tnext_log_size: size of the table is zero, bad param')
+
+if 'gvmi' in ste:
+    print('\tgvmi: ' + ste['gvmi'])
+
+if 'next_table_base_63_48' in ste:
+    print('\tnext_gvmi: ' + ste['next_table_base_63_48'])
+
+if int(sys.argv[4], 16) == 1:
+    print('\n\tverboose:')
+    for k in ste:
+        if (k != 'tag' and k != 'actions') and int(ste[k], 16) != 0:
+            print('\t\t' + k + ': ' + str(ste[k]))
+        if k == 'tag' or k == 'actions':
+            print('\t\t' + k + ':')
+            for l in ste[k]:
+                print('\t\t\t' + l + ': ' + str(ste[k][l]))
+print('\n##############################################\n')
