Mellanox/mlx_steering_dump

commit 774d1fd28268594c9bad2e67e252eb94caaa7247

Add support for single flow dump feature
Add one arg -flowptr for the rte_flow_ptr and user query single rule info by it.
Construct flowptr in iovec.

Signed-off-by: Haifei Luo <haifeil@nvidia.com>

src/dr_trigger.py
@@ -3,6 +3,7 @@ import sys
 import os
 import struct
 from ctypes import *
+import binascii
 
 # struct iovec {
 #    void  *iov_base;    /* Starting address */
@@ -50,12 +51,28 @@ def CMSG_LEN(c_len):
     return c_size_t(sizeof(cmsghdr) + c_len)
 
 
-def fd_msg(fd, port, prevent_py_gc):
+def fd_msg(flow_ptr,fd, port, prevent_py_gc):
     fd = c_int(fd)
-    port = bytearray(chr(port), 'utf-8') + bytearray('\0', 'utf-8')
+   
+    if (flow_ptr != 0):      
+	if (flow_ptr > 0xFFFFFFFF):
+	    print('too large flow ptr ,exit!')
+	    sys.exit(1)
+
+	b = hex(flow_ptr) 
+    	b = b[2:] 
+ 	count=len(b)
+	while (count < 16) :
+		b ='0'+ b
+		count = count + 1	
+
+	c = binascii.a2b_hex(b)   
+	port = bytearray(chr(port), 'utf-8') + c  + bytearray('\0', 'utf-8')
+    else:
+	port = bytearray(chr(port), 'utf-8') + bytearray('\0', 'utf-8')
 
     # create c bytes buffer of size iov_len that contains the port as string
-    iov_len = c_int * 20
+    iov_len = c_int * 40
     iov_base = iov_len(*port)
 
     ptr_iovec = POINTER(iovec)
@@ -83,7 +100,7 @@ def connect_to_server(server_pid):
         sys.exit(1)
     return sock
 
-def request_dump(sock, port, dump_file):
+def request_dump(sock, port, dump_file, flow_ptr):
     try:
         #link to c code of sendmsg
         libc = CDLL('libc.so.6')
@@ -99,7 +116,7 @@ def request_dump(sock, port, dump_file):
     prevent_py_gc = {}
 
     sock_num = c_int(sock.fileno())
-    msg = fd_msg(dump_file.fileno(), port, prevent_py_gc)
+    msg = fd_msg(flow_ptr,dump_file.fileno(), port, prevent_py_gc)
 
     ret = c_sendmsg(sock_num, msg, 0)
     if ret == -1:
@@ -107,12 +124,14 @@ def request_dump(sock, port, dump_file):
 
     sock.recv(1024)
 
-def trigger_dump(s_pid, s_port, path):
+def trigger_dump(s_pid, s_port, path, s_flow_ptr):
     global port
     global server_pid
-
+    global flow_ptr
+   
     port = s_port
     server_pid = s_pid
+    flow_ptr = s_flow_ptr
 
     try:
         dump_file = open(path , 'w')
@@ -121,7 +140,7 @@ def trigger_dump(s_pid, s_port, path):
         sys.exit(1)
 
     sock = connect_to_server(server_pid)
-    request_dump(sock, port, dump_file)
+    request_dump(sock, port, dump_file, flow_ptr)
     dump_file.close() 
     sock.close()
     return path 

mlx_steering_dump_parser.py
@@ -220,6 +220,7 @@ def parse_args():
     parser.add_argument('-r', action='store_true', default=False, dest='raw', help='raw output')
     parser.add_argument('-c', action='store_true', default=False, dest='colored', help='colored output')
     parser.add_argument('-port', dest="dpdk_port", type=int, default=0, help='Trigger DPDK app <PORT> (must provide PID with -p)')
+    parser.add_argument('-flowptr', dest="flow_ptr",type=int, default=0,help='dump single rule by rte_flow_pointer')
     parser.add_argument('-version', action='store_true', default=False, dest='version', help='show version')
     return parser.parse_args()
 
@@ -236,7 +237,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     if (args.dpdk_pid > 0):
-        if dr_trigger.trigger_dump(args.dpdk_pid, args.dpdk_port, args.FILEPATH) is None:
+        if dr_trigger.trigger_dump(args.dpdk_pid, args.dpdk_port, args.FILEPATH, args.flow_ptr) is None:
             sys.exit(-1)
     domain_obj = None
     with open(args.FILEPATH) as csv_file:

commit aea12c7b646044d5cf92a339f39fbc2d73f08490

Fix mixed spaces and tabs error in dr_trigger.py
Fixes: 774d1fd28268 ("Add support for single flow dump feature")

src/dr_trigger.py
@@ -53,23 +53,23 @@ def CMSG_LEN(c_len):
 
 def fd_msg(flow_ptr,fd, port, prevent_py_gc):
     fd = c_int(fd)
-   
-    if (flow_ptr != 0):      
-	if (flow_ptr > 0xFFFFFFFF):
-	    print('too large flow ptr ,exit!')
-	    sys.exit(1)
 
-	b = hex(flow_ptr) 
-    	b = b[2:] 
- 	count=len(b)
-	while (count < 16) :
-		b ='0'+ b
-		count = count + 1	
+    if (flow_ptr != 0):
+        if (flow_ptr > 0xFFFFFFFF):
+            print('too large flow ptr ,exit!')
+            sys.exit(1)
 
-	c = binascii.a2b_hex(b)   
-	port = bytearray(chr(port), 'utf-8') + c  + bytearray('\0', 'utf-8')
+        b = hex(flow_ptr)
+        b = b[2:]
+        count=len(b)
+        while (count < 16):
+            b ='0'+ b
+            count = count + 1
+
+        c = binascii.a2b_hex(b)
+        port = bytearray(chr(port), 'utf-8') + c  + bytearray('\0', 'utf-8')
     else:
-	port = bytearray(chr(port), 'utf-8') + bytearray('\0', 'utf-8')
+        port = bytearray(chr(port), 'utf-8') + bytearray('\0', 'utf-8')
 
     # create c bytes buffer of size iov_len that contains the port as string
     iov_len = c_int * 40