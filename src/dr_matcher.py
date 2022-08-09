Mellanox/mlx_steering_dump

commit 0117879da28a9f79864bf8047d93a25963ecf14e

Fix parsing of matcher builder
Fix dr_dump_matcher_builder - add missing index

Signed-off-by: Yevgeny Kliteynik <kliteyn@nvidia.com>

src/dr_matcher.py
@@ -160,7 +160,7 @@ class dr_dump_matcher_rx_tx(dr_obj):
 
 class dr_dump_matcher_builder(dr_obj):
     def __init__(self, data):
-        keys = ["dr_dump_rec_type", "matcher_id", "is_rx", "lu_type", "definer_id"]
+        keys = ["dr_dump_rec_type", "matcher_id", "index", "is_rx", "lu_type", "definer_id"]
         self.data = dict(zip(keys, data + [None] * (len(keys) - len(data))))
 
     def dump_string(self):

commit daa3a5741b8e594e476535238c08a84e3753b717

Fix parsing of matcher builder for older dumps
Fixing dr_dump_matcher_builder with missing index broke
parsing of older dumps - fixing this.

Fixes: 0117879da28a ("Fix parsing of matcher builder")
Reported-by: Brent Schartung <bschartung@nvidia.com>
Signed-off-by: Muhammad Sammar <muhammads@nvidia.com>
Signed-off-by: Yevgeny Kliteynik <kliteyn@nvidia.com>

src/dr_matcher.py
@@ -160,7 +160,12 @@ class dr_dump_matcher_rx_tx(dr_obj):
 
 class dr_dump_matcher_builder(dr_obj):
     def __init__(self, data):
+        ofed_keys = ["dr_dump_rec_type", "matcher_id", "is_rx", "lu_type", "definer_id"]
         keys = ["dr_dump_rec_type", "matcher_id", "index", "is_rx", "lu_type", "definer_id"]
+
+        if len(data) != len(keys):
+            keys = ofed_keys
+
         self.data = dict(zip(keys, data + [None] * (len(keys) - len(data))))
 
     def dump_string(self):