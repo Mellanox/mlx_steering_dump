d6d51313c6a96b124f81201e26cf2fea67bf9364
Erez Shitrit <erezsh@nvidia.com>
2024-04-01 09:25:59 +0300

    HWS: support dumping FDB sub domains
    Sub domains of FDB are: FDB_RX, FDB_TX and FDB_UNIFIED, now after the
    user can allocate them we will print them in the dump as well.
    
    Signed-off-by: Erez Shitrit <erezsh@nvidia.com>

--- hws/src/dr_rule.py
+++ hws/src/dr_rule.py
@@ -84,6 +84,17 @@ def dr_parse_rules(matcher, verbosity, tabs):
             (DR_TBL_TYPE_NIC_RX, matcher.get_fw_ste_0_index(), matcher.get_ste_arrays(ste_1=False)),
             (DR_TBL_TYPE_NIC_TX, matcher.get_fw_ste_1_index(), matcher.get_ste_arrays(ste_0=False)),
         ],
+        DR_TBL_TYPE_FDB_RX: [
+            (DR_TBL_TYPE_NIC_RX, matcher.get_fw_ste_0_index()),
+            (DR_TBL_TYPE_NIC_TX, matcher.get_fw_ste_1_index()),
+        ],
+        DR_TBL_TYPE_FDB_TX: [
+            (DR_TBL_TYPE_NIC_RX, matcher.get_fw_ste_0_index()),
+            (DR_TBL_TYPE_NIC_TX, matcher.get_fw_ste_1_index()),
+        ],
+        DR_TBL_TYPE_FDB_UNIFIED: [
+            (DR_TBL_TYPE_NIC_RX, matcher.get_fw_ste_0_index()),
+        ],
     }
     tbl_type = _db._tbl_type_db.get(matcher.data.get("tbl_id"))
     dumps = tbl_type_to_dumps[tbl_type]
