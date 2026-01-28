Mellanox/mlx_steering_dump

commit d94520b267d094f3638ff1ef2d2d8e68f29dacd7

HWS: Write whether the match template is low level or not
If the match template is low level, write it as part of the properties.
Example:

    Matcher 0x4e7c60:
        Attr: priority 0x0, log_sz 2X2
        Match_template 0x4e7d60 (low level):
            Match fields:
                src_ip_o: 0xffffffff

Signed-off-by: Yevgeny Kliteynik <kliteyn@nvidia.com>

hws/src/dr_matcher.py
@@ -437,7 +437,8 @@ class dr_parse_matcher_attr():
 
 class dr_parse_matcher_match_template():
     def __init__(self, data):
-        keys = ["mlx5dr_debug_res_type", "id", "matcher_id", "fc_sz", "flags", "fcr_sz", "fcc_sz"]
+        keys = ["mlx5dr_debug_res_type", "id", "matcher_id", "fc_sz", "flags",
+                "fcr_sz", "fcc_sz", "is_ll"]
         self.data = dict(zip(keys, data + [None] * (len(keys) - len(data))))
         self.match_definer = None
         self.range_definer = None
@@ -454,14 +455,20 @@ class dr_parse_matcher_match_template():
         if self.data.get("fcr_sz") != "0" or self.data.get("fcc_sz") != "0":
             self.second_ste_present = True
 
+        if self.data.get("is_ll") == None:
+            self.data["is_ll"] = "0"
+
     def dump_str(self, tabs, verbosity):
         _tabs = tabs + TAB
         __tabs = _tabs + TAB
         _str = ':'
         if self.match_definer != None:
+            if (self.data["is_ll"] == "1"):
+                _str = ' (low level):'
+
             definer_str = self.match_definer.dump_fields()
             if len(definer_str) != 0:
-                _str = ':\n' + _tabs + 'Match fields:\n' + __tabs + definer_str
+                _str += '\n' + _tabs + 'Match fields:\n' + __tabs + definer_str
                 _str = _str.replace(', ', '\n' + __tabs)
 
             range_definer_str = ''

commit 67aa42c3d3c1ac771993d881d6f0b1e8b02d7265

HWS: print low-level API for MTs in a jason-like manner
The commit that added the low-level match template print
did it in a way that is not in line with the overall
file format.
Fixing this to be more jason-like:

  Matcher 0x4e7c60:
      Attr: priority 0x0, log_sz 2X2
      Match_template 0x4e7d60:
          Low_level: yes
          Match fields:
	      ....

Fixes: d94520b267d0 ("HWS: Write whether the match template is low level or not")
Signed-off-by: Yevgeny Kliteynik <kliteyn@nvidia.com>

hws/src/dr_matcher.py
@@ -464,7 +464,7 @@ class dr_parse_matcher_match_template():
         _str = ':'
         if self.match_definer != None:
             if (self.data["is_ll"] == "1"):
-                _str = ' (low level):'
+                _str += '\n' + _tabs + 'Low_level: yes'
 
             definer_str = self.match_definer.dump_fields()
             if len(definer_str) != 0: