Mellanox/mlx_steering_dump

commit a5517a045842b9ba746ba4d391d3f3463c4f4961

HWS: Support dumping of insert by pointer action data
Support dumping of insert by pointer action data, if "extra_hw_res" input
param of value "arg" or "all" was passed.
The output format is:
data: 0000: 01 50 56 19 20 a7 24 8a  07 a5 28 c8 08 00 45 00
      0010: 03 f2 00 00 40 00 40 11  30 f4 01 01 01 02 02 02
      0020: 02 03 04 d2 12 b5 03 de  14 de 08 10 20 30 76 ad
      0030: f1 11

Signed-off-by: Hamdan Igbaria <hamdani@nvidia.com>

hws/src/dr_action.py
@@ -55,9 +55,29 @@ def dr_action_insert_inline_parser(action_arr, index):
 def dr_action_insert_pointer_parser(action_arr, index):
     action_dw_0 = action_arr[index]
     action_dw_1 = action_arr[index + 1]
-    action = dr_parse_insert_by_pointer_action(action_dw_0, action_dw_1)
+    action = dr_parse_insert_by_pointer_action(action_dw_0, action_dw_1, True)
+    sz_bytes = action.get("size")
+    action["size"] = '%s Bytes' % hex(sz_bytes)
+    str_arr = [action_pretiffy(action)]
 
-    return (2, [action_pretiffy(action)])
+    if _config_args.get("extra_hw_res_arg") == True:
+        arg_index = hex(action.get("pointer"))
+        arg_str = _db._argument_db.get(arg_index)
+        if (arg_str is not None) and (len(arg_str) >= (sz_bytes * 2)):
+            data = f'{' ':16}data: 0000:'
+            for i in range(sz_bytes):
+                if i % 0x10 == 0 and i > 0:
+                    str_arr.append(f'{data}\n')
+                    data = f'{' ':22}{i:04x}:'
+                elif i % 0x8 == 0 and i > 0:
+                    data += ' '
+
+                data += f' {arg_str[2 * i]}{arg_str[(2 * i) + 1]}'
+
+                if i == sz_bytes - 1:
+                    str_arr.append(f'{data}\n')
+
+    return (2, str_arr)
 
 def dr_action_accelerated_modify_list_parser(action_arr, index):
     verbose = _config_args.get("verbose")

hws/src/dr_common.py
@@ -165,6 +165,7 @@ DR_HL_INNER = '_i'
 
 MEM_MODE_MIN_MFT_VERSION = 'mft 4.20.0-00'
 
+STC_ACTION_INSERT_HEADER = '0b'
 STC_ACTION_HEADER_MODIFY_LIST = '0e'
 STC_ACTION_TYPE_FLOW_COUNTER = '14'
 STC_ACTION_JUMP_TO_STE_TABLE = '80'

hws/src/dr_common_functions.py
@@ -250,10 +250,11 @@ def dr_parse_insert_inline_action(action_dw_0, action_dw_1, parse_value=True):
     return action
 
 
-def dr_parse_insert_by_pointer_action(action_dw_0, action_dw_1):
+def dr_parse_insert_by_pointer_action(action_dw_0, action_dw_1, int_size_bytes=False):
     action = {"type" : "Insert pointer"}
     start_anchor = 0
     start_offset = 0
+    sz_bytes = 0
 
     if _config_args.get("cx8"):
         start_anchor = int(action_dw_0[8 : 15], 2)
@@ -267,7 +268,12 @@ def dr_parse_insert_by_pointer_action(action_dw_0, action_dw_1):
     #Offset in words granularity
     action["start_offset"] = '%s Bytes' % hex(start_offset * 2)
     #Size in words granularity
-    action["size"] = '%s Bytes' % hex(int(action_dw_0[23 : 29], 2) * 2)
+    sz_bytes = int(action_dw_0[23 : 29], 2) * 2
+    if int_size_bytes:
+        action["size"] = sz_bytes
+    else:
+        action["size"] = '%s Bytes' % hex(sz_bytes)
+
     action["attributes"] = int(action_dw_0[29 : 32], 2)
     action["pointer"] = int(action_dw_1[0 : 32], 2)
 

hws/src/dr_dump_hw.py
@@ -95,7 +95,7 @@ def parse_fw_stc_rd_bin_output(stc_index, load_to_db, file):
                     if _id not in _db._fw_ste_indexes_arr:
                         _db._fw_ste_indexes_arr.append(_id)
 
-                elif obj.get("type") == 'MODIFY_LIST':
+                elif obj.get("type") == 'MODIFY_LIST' or obj.get("type") == 'INSERT_HEADER':
                     _db._arg_obj_indexes_dic[obj.get("id")] = ''
 
                 elif obj.get("type") == 'FLOW_COUNTER':

hws/src/dr_hw_resources.py
@@ -95,6 +95,7 @@ class dr_parse_stc():
 #according to stc obj param
 stc_param_id_loc_dic = {
     STC_ACTION_HEADER_MODIFY_LIST: {'type': 'MODIFY_LIST', 'loc': (8, 16)},
+    STC_ACTION_INSERT_HEADER: {'type': 'INSERT_HEADER', 'inline_data': (1, 2), 'loc': (8, 16)},
     STC_ACTION_JUMP_TO_STE_TABLE: {'type': 'FW_STE_TABLE', 'loc': (0, 8)},
     STC_ACTION_JUMP_TO_TIR: {'type': 'TIR', 'loc': (2, 8)},
     STC_ACTION_JUMP_TO_FLOW_TABLE: {'type': 'FT', 'loc': (2, 8)},
@@ -118,6 +119,10 @@ def dr_parse_fw_stc_action_get_obj_id(raw, stc_param_start_ofset, stc_param_end_
            (action_type == STC_ACTION_TYPE_ALLOW) or \
            (action_type == STC_ACTION_JUMP_TO_QP):
             return {"type": obj.get("type"), "id": ''}
+        elif (action_type == STC_ACTION_INSERT_HEADER):
+            inline_loc = obj.get("inline_data")
+            if int(stc_param[inline_loc[0]:inline_loc[1]], 16) & 0x4 == 0x4:
+                return None
 
         id_loc = obj.get("loc")
         return {"type": obj.get("type"), "id": hex(int(stc_param[id_loc[0]:id_loc[1]], 16))}

commit 1afae45f212b99be02b9eee9872e6610d76fac25

HWS: fix error in parsing insert by pointer action data
Fix syntax error in parsing newely added insert by pointer action data.

Fixes: a5517a045842 ("HWS: Support dumping of insert by pointer action data")
Signed-off-by: Yevgeny Kliteynik <kliteyn@nvidia.com>

hws/src/dr_action.py
@@ -64,11 +64,11 @@ def dr_action_insert_pointer_parser(action_arr, index):
         arg_index = hex(action.get("pointer"))
         arg_str = _db._argument_db.get(arg_index)
         if (arg_str is not None) and (len(arg_str) >= (sz_bytes * 2)):
-            data = f'{' ':16}data: 0000:'
+            data = f'{" ":16}data: 0000:'
             for i in range(sz_bytes):
                 if i % 0x10 == 0 and i > 0:
                     str_arr.append(f'{data}\n')
-                    data = f'{' ':22}{i:04x}:'
+                    data = f'{" ":22}{i:04x}:'
                 elif i % 0x8 == 0 and i > 0:
                     data += ' '
 